0d6e116dc9f451fe7a5538637f217c6aed870f95
Shameer-U <shameerushahul@gmail.com>
2022-05-15 17:33:14 +0530

    set up goalsController and routes

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
+// router.get('/', (req, res) => {
+//     res.status(200).json({message: 'Get goals'})
+// })
+
+// router.get('/', getGoals)
+
+// router.post('/', setGoal)
+
+router.route('/').get(getGoals).post(setGoal)
+
+// router.put('/:id', updateGoal)
+
+// router.delete('/:id', deleteGoal)
+
+router.route('/:id').put(updateGoal).delete(deleteGoal)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// app.get('/api/goals', (req, res) => {
+//     //res.send('Get goals')
+//     res.status(200).send({message: 'Get goals'})
+// })
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
