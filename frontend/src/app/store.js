bed17cae3d924da6013c4ebf88eb73fba649481d
Shameer U Shahul <shameerushahul@gmail.com>
2024-01-07 01:37:33 +0530

    persist state on page refresh using redux-persist

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
 import goalReducer from '../features/goals/goalSlice'
 import singleGoalReducer from '../features/singleGoal/singleGoalSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     goals: goalReducer,
+//     singleGoal: singleGoalReducer
+//   },
+// });
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const reducer = combineReducers({
+  auth: authReducer,
+  goals: goalReducer,
+  singleGoal: singleGoalReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    goals: goalReducer,
-    singleGoal: singleGoalReducer
-  },
+  reducer: persistedReducer
 });
+
+export const persistor = persistStore(store);
